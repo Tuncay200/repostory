c46b52d76df4ef11334e58df128c486e106112f9
Tuncay200 <tuncayadisirinli528@gmail.com>
2024-05-15 19:15:23 +0400

    test commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public  static void main(){
         int a=5;
         int b=10;
-        int temp= a + b;
+        int temp= a + b +5;
         System.out.println(temp);
     }
 }
